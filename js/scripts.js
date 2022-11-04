4278be8653f34c48232df4353f85ac448297146c
Brian Noh <noh24@ymail.com>
2022-11-04 12:08:14 -0400

    Add  Pizza.prototype.addPrice() to clear test1

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,10 @@ Pizza.prototype.addSize = function(size) {
 Pizza.prototype.addTopping = function(topping) {
   this.topping = topping;
 }
+
+Pizza.prototype.addPrice = function(price) {
+  this.price = price;
+}
 //make pizza
   //add toppings
   //choose size
